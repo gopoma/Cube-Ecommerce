187ee5fc2afd9dff905e8be8f3b37eacbcaeb36b
Gustavo <gordono@unsa.edu.pe>
2022-05-17 23:28:55 -0500

    Project Template

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+const {port} = require("./config");
+const {connection} = require("./config/db");
+
+const app = express();
+
+connection();
+
+// Cookies. Diferentes tipos de cookies. HTTP Only Cookie
+
+// Utilizando middleware
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  return res.json({message:"Educational-Ecommerce"});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on: http://localhost:${port}`);
+});
