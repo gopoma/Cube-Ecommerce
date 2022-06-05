c0c08342afc78e56a85ad400c3f16a3ad6a5b8ee
Gustavo <gordono@unsa.edu.pe>
2022-06-05 00:45:53 -0500

    Removed backendHeroku because it is the app for production and has to persist the .env

--- backend/helpers/duplicatedError.js
+++
@@ -1,9 +0,0 @@
-function duplicatedError(error) {
-  const errors = Object.keys(error).map(field => ({
-    [field]: `The ${field} '${error[field]}' is already in use`
-  }));
-
-  return errors;
-}
-
-module.exports = duplicatedError;
