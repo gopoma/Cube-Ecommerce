5a73997a6063eb08077e1d669c1836959b38515a
Gustavo <gordono@unsa.edu.pe>
2022-06-05 00:50:37 -0500

    Making a trampoline

--- backend/helpers/validationError.js
+++
@@ -1,6 +0,0 @@
-function validationError(errors) {
-  const messages = Object.values(errors).map(error => ({message:error.message, field:error.path}));
-  return messages;
-}
-
-module.exports = validationError;
