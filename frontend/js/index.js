850f54db3b8f34e56c2efae908160cd1a91cb644
Gustavo <gordono@unsa.edu.pe>
2022-06-05 01:42:32 -0500

    Added SignUp, LogIn and LogOut interface in the FrontEnd

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,53 @@
-fetch("http://localhost:4000")
-.then(response => response.json())
-.then(data => {console.log(data);})
+const production = false;
+const BASE_URL = production ? "https://still-plateau-02291.herokuapp.com" : "http://localhost:4000"
+const signUpForm = document.querySelector("#signUpForm");
+signUpForm.onsubmit = function() {
+  const url = `${BASE_URL}/api/auth/signup`;
+  const data = {
+    name: signUpForm.name.value,
+    email: signUpForm.email.value,
+    password: signUpForm.password.value
+  };
+  const request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+
+  fetch(url, request)
+  .then(response => response.json())
+  .then(data => {console.log(data);})
+  return false;
+}
+
+const logInForm = document.querySelector("#logInForm");
+logInForm.onsubmit = function() {
+  const url = `${BASE_URL}/api/auth/login`;
+  const credentials = {
+    email: logInForm.email.value,
+    password: logInForm.password.value
+  };
+  const request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(credentials)
+  };
+
+  fetch(url, request)
+  .then(response => response.json())
+  .then(data => {console.log(data);})
+
+  return false;
+}
+
+const btnLogOut = document.querySelector("#btnLogOut");
+btnLogOut.onclick = function() {
+  const url = `${BASE_URL}/api/auth/logout`;
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {console.log(data);})
+}
