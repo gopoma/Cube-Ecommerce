af4c6a044b35ec9c72eb6ae093fb287e9033904c
Gustavo <gordono@unsa.edu.pe>
2022-06-05 01:23:59 -0500

    Solved it by changing localhost to 127.0.0.1 and seeing that it is not the same from the origin in cors

--- backendGoogleCloud/index.js
+++ backendGoogleCloud/index.js
@@ -23,7 +23,7 @@ connection();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
-  origin:["http://localhost:5500"],
+  origin:["http://127.0.0.1:5500"],
   credentials: true
 }));
 app.use(cookies());
